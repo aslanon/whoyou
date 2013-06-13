4be8e5250a761ba8f4a1de7253a558448192f2ac
aslanon <slnnronur@gmail.com>
2013-06-14 00:57:42 +0300

    del file

--- setup.py
+++
@@ -1,5 +0,0 @@
-import os
-print "copying whoyou.py --> /usr/bin"
-os.system("cp whoyou.py /usr/bin")
-print "copying whoyou.desktop  --> /etc/xdg/autostart"
-os.system("cp whoyou.desktop /etc/xdg/autostart")
