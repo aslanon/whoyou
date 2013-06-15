0f4df05275be68e96494bfb20a207ab07a925bc1
aslanon <slnnronur@gmail.com>
2013-06-16 01:39:20 +0300

    added file read

--- wyTwitter.py
+++ wyTwitter.py
@@ -1,5 +1,6 @@
-import urllib, re
-
+import urllib, re, os
+  
+  
 twitter_status = True
 twitter_command = ""
 
@@ -12,14 +13,34 @@ twitterCompile = re.compile('<p class="js-tweet-text tweet-text">(.+?)</p>')
 tweet_text = twitterCompile.findall(twitter.read())
 print "Tweet Command:", "'"+tweet_text[0]+"'"
 
+if os.path.isfile("%s/command" % fileDirectory) == False:
+  file = open("%s/command" % fileDirectory, "w")
+  file.write("open")
+  file.close()    
+
 if tweet_text[0] == "PC:Close":
   twitter_command = "Close"
+  file = open("%s/command" % fileDirectory, "w")
+  file.write("close")
+  file.close()
   print "Closing..."
+  os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
+  
 elif tweet_text[0] == "PC:Open":
   twitter_command = "Open"
+  file =open("%s/command" % fileDirectory,"w")
+  file.write("open")
+  file.close()
   print "Open..."
+  
 else:
   twitter_status = False
   print "Invalid tweet."
+  file =open("%s/command" % fileDirectory).read()
+  if file == "close":
+    print "Closing..."
+    os.system("qdbus org.kde.ksmserver /KSMServer logout 0 2 0")
+  elif file == "open":
+    print "Open..."
   
 #print twitter_status, twitter_command
